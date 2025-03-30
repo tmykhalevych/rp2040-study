64166ccfea0211de272d586d07c4d676c8bfb91d
Taras Mykhalevych <taras.mykhalevych@gmail.com>
2025-03-30 10:07:06 +0000

    feat: Add STUCK() to unreachable

--- src/firmware/common/unreachable.hpp
+++ src/firmware/common/unreachable.hpp
@@ -4,15 +4,33 @@
 
 namespace cmn
 {
+namespace details
+{
 
 [[noreturn]] inline void unreachable()
 {
-    ASSERT(false);
-
     // we're not going to use MSVC compiler, Clang and GCC supported
     __builtin_unreachable();
+}
+
+}  // namespace details
+
+/// @brief Asserts and never returns
+[[noreturn]] inline void unreachable()
+{
+    ASSERT(false);
+    details::unreachable();
+}
+
+/// @brief Stucks in an infinite loop and never returns
+[[noreturn]] inline void stuck()
+{
+    while (true) {
+    }
+    details::unreachable();
 }
 
 }  // namespace cmn
 
 #define UNREACHABLE() cmn::unreachable()
+#define STUCK() cmn::stuck()
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <task.h>
 
 #include <firmware.hpp>
+#include <unreachable.hpp>
 
 #include <cstdio>
 
@@ -15,21 +16,17 @@ int main()
 extern "C" void vApplicationMallocFailedHook()
 {
     std::printf("allocation failure\n");
-    while (true) {
-    }
+    STUCK();
 }
 
-extern "C" void vApplicationStackOverflowHook(TaskHandle_t task_hdl, char* task_name)
+extern "C" void vApplicationStackOverflowHook(TaskHandle_t, char* task_name)
 {
-    (void)task_hdl;
-    std::printf("stach overflow [tsk:%s]\n", task_name);
-    while (true) {
-    }
+    std::printf("stack overflow [tsk:%s]\n", task_name);
+    STUCK();
 }
 
 extern "C" void vAssertCalled(const char* file_name, unsigned line)
 {
     std::printf("assert at %s:%u\n", file_name, line);
-    while (true) {
-    }
+    STUCK();
 }
